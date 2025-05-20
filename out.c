5510fac269a532143d7ae6479320a90a99512f58
Joao-Gabriel05 <jgffaraco@gmail.com>
2025-05-20 10:37:06 -0300

    final

--- out.c
+++ out.c
@@ -2,22 +2,12 @@
 #include <stdio.h>
 
 int main() {
-int a = 5;
-int x = 2;
-printf("%d\n", a);
-printf("%d\n", x);
-printf("[DEBUG] Entering debug block\n");
-int z = 0;
-printf("Line 1: remember z as %d\n", z);
-printf("Line 2: z = %d, z > (-1) = %d\n", z, z > (-1));
-while (z > (-1)) {
-    z =   (-1);
-  int b = a + x;
-  printf("%d\n", b);
-    a = b * 3;
-  printf("%d\n", a);
+int sum = 0;
+int i = oi;
+while (i <= 5) {
+    sum = sum + i;
+    i = i + 1;
 }
-printf("[DEBUG] Exiting debug block\n");
-printf("%d\n", a + x);
+printf("%d\n", sum);
     return 0;
 }
